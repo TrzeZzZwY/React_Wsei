f21ef04613839b486ce0f06178ccc14026e4b04d
Jan Świder <jasiek.swider@outlook.com>
2023-10-14 22:12:37 +0200

    tailwind added

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,3 +1,3 @@
 export default function MainPage({ body, header }: { body: string, header: string },) {
-    return <div><h1>{body} {header}</h1></div>;
+    return <div><h1 className="text-red-500 font-bold text-2xl">{body} {header}</h1></div>;
 }
